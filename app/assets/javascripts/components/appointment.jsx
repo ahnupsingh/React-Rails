6af6acb544a2130e23ec646847887ffaff5587be
ahnupsingh <anup.singh2071@gmail.com>
2019-09-01 22:44:45 +0545

    Fix class by className

--- app/assets/javascripts/components/appointment.jsx
+++ app/assets/javascripts/components/appointment.jsx
@@ -1,7 +1,7 @@
 class Appointment extends React.Component {
 	render() {
 		return (
-      <div class="appointment">
+      <div className="appointment">
         <h3>{this.props.appointment.title}</h3>
         <p>{formatDate(this.props.appointment.apt_time)}</p>
       </div>
